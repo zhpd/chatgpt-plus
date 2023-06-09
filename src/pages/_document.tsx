9b25d775b534678d07befde874afa727862de7a2
jude <zpd106@sina.com>
2023-06-09 16:13:53 +0800

    fix: fixed google analytics not

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,8 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
+const isProduction = process.env.NODE_ENV === 'production'
+const GOOGLE_GA_ID = process.env.GOOGLE_GA_ID || ''
+
 export default function Document() {
   return (
     <Html lang="zh" data-theme="light">
@@ -16,6 +19,24 @@ export default function Document() {
           `,
           }}
         />
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        {isProduction && GOOGLE_GA_ID && (
+          <>
+            <script async src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_GA_ID}`} />
+            <script
+              id="google-analytics"
+              dangerouslySetInnerHTML={{
+                __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag("js", new Date());
+                gtag("config", "${GOOGLE_GA_ID}");
+                conosle.log('google-analytics loaded')
+              `,
+              }}
+            ></script>
+          </>
+        )}
       </Head>
       <body>
         <Main />
